c0831ebfd4a0dfc7af23c36f8c58c2631c2d2d02
Sarah <ennis7@mail.nmc.edu>
2024-03-07 20:25:42 -0500

    3/7

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericAnimalClass;
+public class Animal <T>
+{
+    public T data;
+    public Animal (T data)
+    {
+        this.data = data;
+        Console.WriteLine("Data passed: " + this.data);
+    }
+    public T getAnimal()
+    {
+        return data;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Animal<string> animalName = new Animal<string>("Snowman");
+       Animal<string> animalHabitat = new Animal<string>("I like to eat hay");
+       Animal<bool> endangered = new Animal<bool>(false);
+       Animal<bool> extinct = new Animal<bool>(false);
+       Animal<int> averageLifespan = new Animal<int>(30);
+       Console.ReadKey();
+    }
+}
+
